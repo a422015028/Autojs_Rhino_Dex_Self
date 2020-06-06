f78c6e45f88eccfe59db8a8277846d48566ce652
xxxue <1659809758@qq.com>
2020-06-06 18:24:09 +0800

    取消注释 "强制UTF-8"

--- toolsrc/org/mozilla/javascript/tools/jsc/Main.java
+++ toolsrc/org/mozilla/javascript/tools/jsc/Main.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -307,8 +308,8 @@ public class Main {
             addError("msg.jsfile.not.found", absPath);
             return null;
         }
-
-        //characterEncoding = StandardCharsets.UTF_8.toString();
+        //强制 UTF-8
+        characterEncoding = StandardCharsets.UTF_8.toString();
 
         try {
             return (String)SourceReader.readFileOrUrl(absPath, true, characterEncoding);
